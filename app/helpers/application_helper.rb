2a57c663af6e42a4eec2fa9f0f169c0b2634f452
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-29 10:51:02 -0700

    resource update splits long URIs nicely for display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def breakable_uri(uri, breaker='<wbr/>')
+    temp = uri
+    result = []
+    schema = temp.match %r{\Ahttps?://}i
+    if schema
+      schema = schema[0]
+      result.push schema
+      temp = temp[schema.length..-1]
+    end
+    while (m = temp.match %r{\A[^/_%]+(?:/|_|%[0-9]{2})+}) do
+      m = m[0]
+      result.push m
+      temp = temp[m.length..-1]
+    end
+    
+    result.join breaker
+  end
+  
+  def breakable_email(email, breaker='<wbr/>')
+    temp = email
+    result = []
+    while (m = temp.match %r{\A[^/_%@]+(?:@|/|_|%[0-9]{2})+}) do
+      m = m[0]
+      result.push m
+      temp = temp[m.length..-1]
+    end
+    
+    result.join breaker
+  end
 end
