dc166f18c4c0b3a50aaf96a9a3d58fc1a7c1906c
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-06-30 15:59:43 -0700

    rm: many validations from DueDate model

--- app/models/due_date.rb
+++ app/models/due_date.rb
@@ -1,15 +1,14 @@
 class DueDate < ActiveRecord::Base
-  BRANCH_RGX = /\A(?:master|[0-9]{3}_(?:[0-9]+_)*release)\z/
-  BRANCH_ERR = "should be either 'master' or something like '013_0_1_release'"
-  VERSION_RGX = /\Av[1-9][0-9]*(?:\.(?:0|[1-9][0-9]*)){2,3}\z/
-  VERSION_ERR = "should begin with a 'v' and use periods to separate version segments, like 'v13.0.1'"
+  # BRANCH_RGX = /\A(?:master|[0-9]{3}_(?:[0-9]+_)*release)\z/
+  # BRANCH_ERR = "should be either 'master' or something like '013_0_1_release'"
+  # VERSION_RGX = /\Av[1-9][0-9]*(?:\.(?:0|[1-9][0-9]*)){2,3}\z/
+  # VERSION_ERR = "should begin with a 'v' and use periods to separate version segments, like 'v13.0.1'"
   DATE_RGX = %r{\A(?:0[1-9]|1[0-2])/(?:0[1-9]|[12][0-9]|3[01])/(?:19|20)(?:[0-9]{2})\z}
   DATE_ERR = "should use MM/DD/YYYY format, like 03/30/2016"
+  
   validates :branch_name, 
-    presence: true, 
-    format: {with: BRANCH_RGX, message: BRANCH_ERR}
-  validates :target_version,
-    format: {with: VERSION_RGX, message: VERSION_ERR}
+    presence: true
   validates :due,
     format: {with: DATE_RGX, message: DATE_ERR}
+
 end
