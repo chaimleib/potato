6b247dc555243dd14e106d01f236d65676844a24
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-06 13:45:54 -0700

    rm: cruft

--- app/assets/javascripts/timeago.js
+++ app/assets/javascripts/timeago.js
@@ -1,7 +1,3 @@
 jQuery.timeago.settings.allowFuture = true;
 jQuery.timeago.settings.strings['en'].prefixFromNow = 'in';
 jQuery.timeago.settings.strings['en'].suffixFromNow = null;
-
-jQuery('.timeago').livequery(function() {
-	jQuery(this).timeago();
-});
