c16851bf7bfd8800dc41ffa7f5f501909466df52
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-06 14:30:26 -0700

    timeago.js: better scoping

--- app/assets/javascripts/timeago.js
+++ app/assets/javascripts/timeago.js
@@ -1,3 +1,7 @@
-jQuery.timeago.settings.allowFuture = true;
-jQuery.timeago.settings.strings['en'].prefixFromNow = 'in';
-jQuery.timeago.settings.strings['en'].suffixFromNow = null;
+(function() {
+	var ta = $.timeago;
+
+	ta.settings.allowFuture = true;
+	ta.settings.strings['en'].prefixFromNow = 'in';
+	ta.settings.strings['en'].suffixFromNow = null;
+})();
