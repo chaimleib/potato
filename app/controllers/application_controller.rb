124cd5cd0417e299ed05b0f21545726be28e3044
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-14 16:06:25 -0700

    application_controller add: shown_to, editable_by, model, controller_base_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_crumb("Home", '/')
   
+  class_attribute :shown_to, :editable_by, :model
+  self.shown_to = [:all]
+  self.editable_by = [:administrators]
+  self.model = nil
+
   def self.controller_display_name
     self.controller_name.underscore.humanize.capitalize
   end
@@ -14,5 +19,13 @@ class ApplicationController < ActionController::Base
 
   def action_display_name(name=action_name)
     name.humanize.capitalize
+  end
+
+  def self.controller_base_path
+    "/#{controller_name}"
+  end
+
+  def controller_base_path
+    self.class.controller_base_path
   end
 end
