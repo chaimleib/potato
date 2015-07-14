965bf5134b57e8b6a1075460618e3968a4e70eba
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-13 19:22:05 -0700

    factory due_dates.rb: invalid date format

--- spec/factories/due_dates.rb
+++ spec/factories/due_dates.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :due_date do
     branch_name "MyString"
-target_version "MyString"
-due "MyString"
+    target_version "MyString"
+    due "10/20/2015"
   end
-
 end
