643b8554d61fcb49dcdfe07e6a3b34a9f1683261
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-13 19:24:12 -0700

    spec overview.html.haml_spec.rb add: renders successfully

--- spec/views/potato/overview.html.haml_spec.rb
+++ spec/views/potato/overview.html.haml_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "potato/overview.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'renders successfully' do
+    context = {
+      user: Rails.application.secrets.jira['username'],
+      errors: {}
+    }
+    assign :context, context
+    
+    render
+
+    expect(rendered).to match %r{<table [^>]*data-url=['"]/potato/overview\?utf8=%E2%9C%93&amp}
+    # is_expected.to render('potato/overview.html.haml')
+  end
 end
