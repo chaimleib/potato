71066de7204247af5974c9ad119f2d1f12e55a0d
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-15 14:45:03 -0700

    seeds: should not reference ENV, only secrets.yml should do that

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ ResourceUpdate.delete_all
 User.delete_all
 User.create(
   fname: "Admin", 
-  lname: "Master", 
-  email: ENV["ROOT_USER", 
+  lname: "Root", 
+  email: Rails.application.secrets.root_user, 
   password_digest: "$2a$10$fbajvb4YUwXO8rdwH9O6hepZ6mLwCqbrPB2Y3Ke2JBPjTlzAulPdC"
 )
