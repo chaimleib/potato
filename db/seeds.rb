954a50ed4401f987424bf37dada85072273d7de7
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-14 19:33:15 -0700

    db seeds: use ENV['ROOT_USER']

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 CodeFreeze.delete_all
 ResourceUpdate.delete_all
 User.delete_all
-User.create("fname"=>"Admin", "lname"=>"Master", "email"=>"admin_master@example.com", "password_digest"=>"$2a$10$fbajvb4YUwXO8rdwH9O6hepZ6mLwCqbrPB2Y3Ke2JBPjTlzAulPdC")
+User.create(
+  fname: "Admin", 
+  lname: "Master", 
+  email: ENV["ROOT_USER", 
+  password_digest: "$2a$10$fbajvb4YUwXO8rdwH9O6hepZ6mLwCqbrPB2Y3Ke2JBPjTlzAulPdC"
+)
