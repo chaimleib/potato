8e4e80bc45949a2d1e1d17726b88231f48c32d96
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-15 15:09:29 -0700

    user_permissions show,index: use helper.admin_notes

--- app/helpers/user_permissions_helper.rb
+++ app/helpers/user_permissions_helper.rb
@@ -1,2 +1,15 @@
 module UserPermissionsHelper
+  RIGHT_ARROW = "\u2192"
+
+  def self.admin_notes(user_permission)
+    admin_notes = []
+    user = user_permission.user
+    if user.is_root?
+      admin_notes << 'root'
+    end
+    if !!user_permission.is_admin != user.is_admin?
+      admin_notes << "user.is_admin? #{RIGHT_ARROW} #{user.is_admin?}"
+    end
+    admin_notes
+  end
 end
