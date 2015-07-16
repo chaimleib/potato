feed08b15c0910c62e3a95f517013c8579a9bab3
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-16 13:14:02 -0700

    set time zone to Pacific

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Potato
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Pacific Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -22,8 +22,5 @@ module Potato
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # config.less.paths << Rails.root.
-    #   join('vendor', 'assets', 'components', 'bootstrap-datetimepicker', 'src', 'less')
-    # config.less.compress = true
   end
 end
