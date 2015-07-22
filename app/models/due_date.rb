57a5665b2c3011cf7892395572ab68bdce175c27
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-22 13:41:34 -0700

    due_date: validate branch_name uniqueness, prevent circular due_ref

--- app/models/due_date.rb
+++ app/models/due_date.rb
@@ -6,15 +6,16 @@ class DueDate < ActiveRecord::Base
   # ISO8601_RGX = /\A(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)\z/
   DATE_RGX = %r{\A(?:0[1-9]|1[0-2])/(?:0[1-9]|[12][0-9]|3[01])/(?:19|20)(?:[0-9]{2})\z}
   DATE_ERR = "should use MM/DD/YYYY format, like 03/30/2016"
-  
+
   belongs_to :due_ref, class_name: 'DueDate', foreign_key: :due_ref_id
   has_many :referencers, class_name: 'DueDate', foreign_key: :due_ref_id
-
   validates :branch_name, 
-    presence: true
+    presence: true,
+    uniqueness: true
   validates :due,
     allow_nil: true,
     format: {with: DATE_RGX, message: DATE_ERR}
+  validate :has_no_circular_refs
 
   def self.for_version(version)
     dd = find_by(branch_name: version)
@@ -33,5 +34,29 @@ class DueDate < ActiveRecord::Base
       return self.due
     end
     DueDate.find(self.due_ref_id).resolve
+  end
+
+  def due_ref_chain
+    # returns a hash of all due_ref links, keyed by branch_name.
+    # If there is a circular reference, the hash will have a nil
+    # key assigned to the due_ref causing it.
+    # a hash is used instead of a list so that membership tests
+    # are constant time.
+    cur = self
+    visited = {}
+    begin
+      if visited.has_key? cur.branch_name
+        visited[nil] = cur
+        return visited
+      end
+      cur = visited[cur.branch_name] = cur.due_ref
+    end while !cur.nil?
+    visited
+  end
+
+  def has_no_circular_refs
+    if due_ref_chain.has_key? nil
+      errors.add(:due_ref_id, "has circular reference chain")
+    end
   end
 end
