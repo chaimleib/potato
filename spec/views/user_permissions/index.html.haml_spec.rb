04ff90c64e4c839c5fac46466f6e2efe3ecc4904
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 15:29:35 -0700

    test edit: user_permissions#index

--- spec/views/user_permissions/index.html.haml_spec.rb
+++ spec/views/user_permissions/index.html.haml_spec.rb
@@ -2,21 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "user_permissions/index", type: :view do
   before(:each) do
-    assign(:user_permissions, [
-      UserPermission.create!(
-        :user => nil,
-        :is_admin => ""
-      ),
-      UserPermission.create!(
-        :user => nil,
-        :is_admin => ""
-      )
-    ])
+    root = User.root_user || FactoryGirl.create(:root_user)
+    @user = FactoryGirl.create(:user)
+    users = [
+      root,
+      @user
+    ]
+    assign :user_permissions, users.map(&:user_permission)
   end
 
   it "renders a list of user_permissions" do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "".to_s, :count => 2
+    assert_select "tr>th", :text => 'User first name'
+    assert_select "tr>th", :text => 'User last name'
+    assert_select "tr>th", :text => 'Is admin'
+    assert_select "tr>td", :text => User.root_user.fname
+    assert_select "tr>td", :text => User.root_user.lname
+    assert_select "tr>td", :text => User.root_user.user_permission.is_admin.to_s
+
+    assert_select "tr>td", :text => @user.fname
+    assert_select "tr>td", :text => @user.lname
+    assert_select "tr>td", :text => @user.user_permission.is_admin.to_s
   end
 end
