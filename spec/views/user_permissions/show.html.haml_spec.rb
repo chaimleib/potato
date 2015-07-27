f26c3c01bb00dca0ace7ebe69ae170e56c2f24cf
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 16:35:35 -0700

    test edit: user_permissions#show

--- spec/views/user_permissions/show.html.haml_spec.rb
+++ spec/views/user_permissions/show.html.haml_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "user_permissions/show", type: :view do
   before(:each) do
-    @user_permission = assign(:user_permission, UserPermission.create!(
-      :user => nil,
-      :is_admin => ""
-    ))
+    root = User.root_user || FactoryGirl.create(:root_user)
+    @user_permission = assign(:user_permission, root.user_permission)
   end
 
-  it "renders attributes in <p>" do
+  it "renders the user_permission in a table" do
     render
-    expect(rendered).to match(//)
-    expect(rendered).to match(//)
+
+    assert_select "tr>th", :text => "User:"
+    assert_select "tr>th", :text => "Is admin:"
+    assert_select "tr>td", :text => @user_permission.user.human_email
+    assert_select "tr>td", :text => /\A\s*#{Regexp.escape(@user_permission.is_admin.to_s)}/
+
   end
 end
