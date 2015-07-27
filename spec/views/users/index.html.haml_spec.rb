6bbdcca26a10ee081eb9076563a3daba3b08f75a
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 16:59:59 -0700

    test edit: users#index

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -2,27 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
   before(:each) do
-    assign(:users, [
-      User.create!(
-        :fname => "Fname",
-        :lname => "Lname",
-        :email => "Email",
-        :password_digest => "Password Digest"
-      ),
-      User.create!(
-        :fname => "Fname",
-        :lname => "Lname",
-        :email => "Email",
-        :password_digest => "Password Digest"
-      )
-    ])
+    root = FactoryGirl.create :root_user
+    @user = FactoryGirl.create :user
+    assign :users, [
+      root,
+      @user
+    ]
   end
 
   it "renders a list of users" do
     render
-    assert_select "tr>td", :text => "Fname".to_s, :count => 2
-    assert_select "tr>td", :text => "Lname".to_s, :count => 2
-    assert_select "tr>td", :text => "Email".to_s, :count => 2
-    assert_select "tr>td", :text => "Password Digest".to_s, :count => 2
+    assert_select "tr" do
+      assert_select "th", 'First name'
+      assert_select "th", 'Last name'
+      assert_select "th", 'E-mail'
+    end
+    assert_select "tr" do
+      assert_select "td", User.root_user.fname
+      assert_select "td", User.root_user.lname
+      assert_select "td", User.root_user.concealed_email
+      assert_select "td", "Permissions"
+      assert_select "td", "Root user"
+    end
+    assert_select "tr" do
+      assert_select "td", @user.fname
+      assert_select "td", @user.lname
+      assert_select "td", @user.concealed_email
+      assert_select "td", "Permissions"
+      assert_select "td", "Root user", false
+    end
   end
 end
