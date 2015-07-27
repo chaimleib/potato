19d5e7245a01e8e827fc11e237f559a85a61dd29
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 16:48:14 -0700

    test edit: users#new

--- spec/views/users/new.html.haml_spec.rb
+++ spec/views/users/new.html.haml_spec.rb
@@ -2,26 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "users/new", type: :view do
   before(:each) do
-    assign(:user, User.new(
-      :fname => "MyString",
-      :lname => "MyString",
-      :email => "MyString",
-      :password_digest => "MyString"
-    ))
+    assign :user, FactoryGirl.build(:user)
   end
 
   it "renders new user form" do
     render
 
     assert_select "form[action=?][method=?]", users_path, "post" do
-
       assert_select "input#user_fname[name=?]", "user[fname]"
-
       assert_select "input#user_lname[name=?]", "user[lname]"
-
       assert_select "input#user_email[name=?]", "user[email]"
-
-      assert_select "input#user_password_digest[name=?]", "user[password_digest]"
+      assert_select "input#user_password[type=password][name=?]", "user[password]"
+      assert_select "input#user_password_confirmation[type=password][name=?]", "user[password_confirmation]"
     end
   end
 end
