d53b048fe4f9335026bb069d2d0cd815fb9214df
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 15:36:26 -0700

    test edit: user_permissions#edit

--- spec/views/user_permissions/edit.html.haml_spec.rb
+++ spec/views/user_permissions/edit.html.haml_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "user_permissions/edit", type: :view do
   before(:each) do
-    @user_permission = assign(:user_permission, UserPermission.create!(
-      :user => nil,
-      :is_admin => ""
-    ))
+    root = User.root_user || FactoryGirl.create(:root_user)
+    @user_permission = root.user_permission
   end
 
   it "renders the edit user_permission form" do
     render
 
     assert_select "form[action=?][method=?]", user_permission_path(@user_permission), "post" do
-
-      assert_select "input#user_permission_user_id[name=?]", "user_permission[user_id]"
+      assert_select "input[disabled=disabled][value=?]", @user_permission.user.human_email
+      assert_select "input#user_id[name=?]", "user_id"
 
       assert_select "input#user_permission_is_admin[name=?]", "user_permission[is_admin]"
     end
