d9910aa010da9530668a8dcf80aa7cbd9d9a0e66
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 17:30:51 -0700

    test edit: due_dates#edit

--- spec/views/due_dates/edit.html.haml_spec.rb
+++ spec/views/due_dates/edit.html.haml_spec.rb
@@ -2,22 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "due_dates/edit", type: :view do
   before(:each) do
-    @due_date = assign(:due_date, DueDate.create!(
-      :branch_name => "MyString",
-      :target_version => "MyString",
-      :due => "MyString"
-    ))
+    @due_date = assign :due_date, FactoryGirl.create(:due_date)
   end
 
   it "renders the edit due_date form" do
     render
 
     assert_select "form[action=?][method=?]", due_date_path(@due_date), "post" do
-
       assert_select "input#due_date_branch_name[name=?]", "due_date[branch_name]"
-
-      assert_select "input#due_date_target_version[name=?]", "due_date[target_version]"
-
+      assert_select "select#due_date_due_ref_id[name=?]", "due_date[due_ref_id]"
       assert_select "input#due_date_due[name=?]", "due_date[due]"
     end
   end
