4413a3134fa7b03d35cd8cd1a654ec48a3b65855
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 09:17:21 -0700

    test edit: resource_updates#index

--- spec/views/resource_updates/index.html.haml_spec.rb
+++ spec/views/resource_updates/index.html.haml_spec.rb
@@ -2,17 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "resource_updates/index", type: :view do
   before(:each) do
-    @resource_update1 = FactoryGirl.build :resource_update
-    @resource_update2 = @resource_update1.dup
-    
-    @resource_update1.save
-    @resource_update2.save
+    FactoryGirl.create(:root_user)
+    @user = FactoryGirl.create(:user)
+    @ru1 = FactoryGirl.create :resource_update
+    @ru2 = FactoryGirl.create :resource_update, user: @user
+    assign :resource_updates, [
+      @ru1,
+      @ru2
+    ]
   end
 
   it "renders a list of resource_updates" do
     render
-    assert_select "tr>td", :text => @resource_update1.name, :count => 2
-    assert_select "tr>td", :text => @resource_update1.source_uri, :count => 2
-    assert_select "tr>td", :text => @resource_update1.user.name, :count => 2
+    assert_select "tr" do
+      assert_select "th", "Name"
+      assert_select "th", "Source URI"
+      assert_select "th", "User"
+      assert_select "th", "", count: 3
+    end
+
+    assert_select "tr>td", :text => @ru1.name, :count => 2
+    assert_select "tr>td", :text => @ru1.source_uri, :count => 2
+    assert_select "tr>td>a", :text => @ru1.user.full_name
+    assert_select "tr>td>a", :text => @ru2.user.full_name
+    assert_select "tr>td>a", "Show", count: 2
+    assert_select "tr>td>a", "Edit", count: 2
+    assert_select "tr>td>a", "Destroy", count: 2
   end
 end
