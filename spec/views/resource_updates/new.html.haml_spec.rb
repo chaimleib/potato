36f4e1be2b1793b7864b82449d9b6611a2a8b033
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 08:58:12 -0700

    test edit: resource_update#new

--- spec/views/resource_updates/new.html.haml_spec.rb
+++ spec/views/resource_updates/new.html.haml_spec.rb
@@ -2,23 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "resource_updates/new", type: :view do
   before(:each) do
-    assign(:resource_update, ResourceUpdate.new(
-      :name => "MyString",
-      :source_uri => "MyText",
-      :user => nil
-    ))
+    @resource_update = ResourceUpdate.new
   end
 
   it "renders new resource_update form" do
     render
 
     assert_select "form[action=?][method=?]", resource_updates_path, "post" do
-
       assert_select "input#resource_update_name[name=?]", "resource_update[name]"
-
       assert_select "textarea#resource_update_source_uri[name=?]", "resource_update[source_uri]"
-
-      assert_select "input#resource_update_user_id[name=?]", "resource_update[user_id]"
+      assert_select "select#resource_update_user_id[name=?]", "resource_update[user_id]"
     end
   end
 end
