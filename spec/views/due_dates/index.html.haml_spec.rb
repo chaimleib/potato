4ca1f7e8be9de50209d1af72a9f7879d96747a87
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 08:37:11 -0700

    test edit: due_dates#index

--- spec/views/due_dates/index.html.haml_spec.rb
+++ spec/views/due_dates/index.html.haml_spec.rb
@@ -2,24 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "due_dates/index", type: :view do
   before(:each) do
-    assign(:due_dates, [
-      DueDate.create!(
-        :branch_name => "Branch Name",
-        :target_version => "Target Version",
-        :due => "Due"
-      ),
-      DueDate.create!(
-        :branch_name => "Branch Name",
-        :target_version => "Target Version",
-        :due => "Due"
-      )
-    ])
+
+    assign :due_dates, [
+      (@dd = FactoryGirl.create(:due_date)),
+      (@rdd = FactoryGirl.create(:ref_due_date, due_ref: @dd))
+    ]
   end
 
   it "renders a list of due_dates" do
     render
-    assert_select "tr>td", :text => "Branch Name".to_s, :count => 2
-    assert_select "tr>td", :text => "Target Version".to_s, :count => 2
-    assert_select "tr>td", :text => "Due".to_s, :count => 2
+    assert_select "tr" do
+      assert_select "th", "Branch name"
+      assert_select "th", "Due"
+      assert_select "th", "", 3
+    end
+    assert_select "tr" do
+      assert_select "td", @dd.branch_name
+      assert_select "td", @dd.due
+      assert_select "td>a", "Show"
+      assert_select "td>a", "Edit"
+      assert_select "td>a", "Destroy"
+    end
+    assert_select "tr" do
+      assert_select "td", @rdd.branch_name
+      assert_select "td", /\A#{Regexp.escape @dd.branch_name}/
+      assert_select "td>a", "Show"
+      assert_select "td>a", "Edit"
+      assert_select "td>a", "Destroy"
+    end
   end
 end
