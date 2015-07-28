c33b76c500422e79ad91b247321c5213742d1532
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 17:27:08 -0700

    test factory due_dates add: :due_date, :ref_due_date

--- spec/factories/due_dates.rb
+++ spec/factories/due_dates.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
   factory :due_date do
-    branch_name "MyString"
-    target_version "MyString"
-    due "10/20/2015"
+    branch_name "_eos"
+    due "07/24/2015"
+    due_ref nil
+
+    trait :ref do
+      branch_name "master"
+      due nil
+      due_ref DueDate.first
+    end
+
+    factory :ref_due_date, traits: [:ref]
   end
 end
