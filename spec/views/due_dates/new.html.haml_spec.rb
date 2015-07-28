dd34d44fa177d7153d39b0a14a437bdfd632004f
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 08:40:30 -0700

    test edit: due_dates#new

--- spec/views/due_dates/new.html.haml_spec.rb
+++ spec/views/due_dates/new.html.haml_spec.rb
@@ -2,23 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "due_dates/new", type: :view do
   before(:each) do
-    assign(:due_date, DueDate.new(
-      :branch_name => "MyString",
-      :target_version => "MyString",
-      :due => "MyString"
-    ))
+    assign :due_date, DueDate.new
   end
 
   it "renders new due_date form" do
     render
 
     assert_select "form[action=?][method=?]", due_dates_path, "post" do
-
       assert_select "input#due_date_branch_name[name=?]", "due_date[branch_name]"
-
-      assert_select "input#due_date_target_version[name=?]", "due_date[target_version]"
-
       assert_select "input#due_date_due[name=?]", "due_date[due]"
+      assert_select "select#due_date_due_ref_id[name=?]", "due_date[due_ref_id]"
     end
   end
 end
