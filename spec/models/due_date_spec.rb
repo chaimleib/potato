e3aedc5c2d5f4387d78d356521ee20110274d2c5
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 19:02:28 -0700

    test edit: due_dates model

--- spec/models/due_date_spec.rb
+++ spec/models/due_date_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe DueDate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid from the factory" do
+    dd = FactoryGirl.create :due_date
+    
+    expect(dd.branch_name).to be_present
+    expect(dd.resolve).to be_present
+    expect(dd.due_ref).to be_nil
+    expect(dd.due).to be_present
+
+    lookup_time = DueDate.for_version dd.branch_name
+    lookup_str = lookup_time.strftime '%m/%d/%Y'
+    expect(dd.resolve).to eq(lookup_str)
+
+  end
+  it "is valid from the factory (with due_ref)" do
+    dd = FactoryGirl.create :due_date
+    rdd = FactoryGirl.create :ref_due_date, due_ref: dd
+
+    expect(rdd.branch_name).to be_present
+    expect(rdd.resolve).to be_present
+    expect(rdd.due_ref).to be_present
+    expect(rdd.due).to_not be_present
+
+    lookup_time = DueDate.for_version rdd.branch_name
+    lookup_str = lookup_time.strftime '%m/%d/%Y'
+    expect(rdd.resolve).to eq(lookup_str)
+  end
+
 end
