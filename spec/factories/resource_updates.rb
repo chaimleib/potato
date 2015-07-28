2f24c4bb14c01f727f7ce70b6cf80bc83fcbfed0
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 08:51:59 -0700

    text factory fix: resource_updates takes a user object, not user id

--- spec/factories/resource_updates.rb
+++ spec/factories/resource_updates.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     name "MyString"
     updated "2015-06-25 13:36:31"
     source_uri "https://example.com/wiki/display/CP/CD+Maintenance+Releases"
-    user 1
+    user User.root_user
   end
-
 end
