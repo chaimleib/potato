528c579160262faabe67c2cfc86b71ba818bbab4
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-28 08:52:40 -0700

    test edit: resource_update#edit

--- spec/views/resource_updates/edit.html.haml_spec.rb
+++ spec/views/resource_updates/edit.html.haml_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "resource_updates/edit", type: :view do
   before(:each) do
-    @resource_update = FactoryGirl.build :resource_update
-    @resource_update.save
+    FactoryGirl.create :root_user
+    @resource_update = FactoryGirl.create :resource_update
   end
 
   it "renders the edit resource_update form" do
@@ -12,7 +12,7 @@ RSpec.describe "resource_updates/edit", type: :view do
     assert_select "form[action=?][method=?]", resource_update_path(@resource_update), "post" do
       assert_select "input#resource_update_name[name=?]", "resource_update[name]"
       assert_select "textarea#resource_update_source_uri[name=?]", "resource_update[source_uri]"
-      assert_select "input#resource_update_user_id[name=?]", "resource_update[user_id]"
+      assert_select "select#resource_update_user_id[name=?]", "resource_update[user_id]"
     end
   end
 end
