fe7536c9a9d167fe490bf6e5437e9374e673c700
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-27 19:00:02 -0700

    factory edit: due_dates; could not set a reference from factory file, must set when calling create

--- spec/factories/due_dates.rb
+++ spec/factories/due_dates.rb
@@ -1,15 +1,14 @@
 FactoryGirl.define do
   factory :due_date do
-    branch_name "_eos"
+    branch_name "master"
     due "07/24/2015"
     due_ref nil
 
     trait :ref do
-      branch_name "master"
-      due nil
-      due_ref DueDate.first
+      branch_name "_eos"
+      due  nil
+      # due_ref nil  # must set from creator
     end
-
     factory :ref_due_date, traits: [:ref]
   end
 end
